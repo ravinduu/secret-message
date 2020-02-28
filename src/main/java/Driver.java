2544a6352ade07f70fb9c9361fc975ab7df76d6c
Ravindu Dilshan <ravindu.dilshan.rd@gmail.com>
2020-02-28 13:03:49 +0530

    Edit files

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,13 +1,17 @@
+import gui.FrontEnd;
+
 public class Driver {
     public static void main(String[] args) {
         CaesarCipher caesarsCipher = new CaesarCipher();
 
-        String message = "tqxxa iadxp";
-        int shift = 12;
+//        String message = "tqxxa iadxp";
+//        int shift = 12;
+//
+//        String output = caesarsCipher.input(message,shift);
+//
+//        System.out.println("Message : "+message+"\tShift: "+shift+"\nCipher : "+output);
 
-        String output = caesarsCipher.input(message,shift);
-
-        System.out.println("Message : "+message+"\tShift: "+shift+"\nCipher : "+output);
+        FrontEnd frontEnd = new FrontEnd();
 
     }
 }
