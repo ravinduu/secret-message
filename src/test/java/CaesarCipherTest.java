afe6a5aeddf6aae34de3e5e19e2522cbfbf2e711
Ravindu Dilshan <ravindu.dilshan.rd@gmail.com>
2020-02-28 11:36:52 +0530

    Add Tests

---
+++ src/test/java/CaesarCipherTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaesarCipherTest {
+    private CaesarCipher caesarCipher = new CaesarCipher();
+    @Test
+    void testForEncode() {
+        assertEquals("hello world",caesarCipher.input("tqxxa iadxp",12));
+    }
+
+    @Test
+    void testForNull() {
+        assertEquals("",caesarCipher.input("",23));
+    }
+}
