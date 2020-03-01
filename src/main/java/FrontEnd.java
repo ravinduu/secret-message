6a8de801f3ebb0102235b431dc7129a375eefff9
Ravindu Dilshan <ravindu.dilshan.rd@gmail.com>
2020-03-01 20:29:35 +0530

    Fix Gui

--- src/main/java/FrontEnd.java
+++ src/main/java/FrontEnd.java
@@ -1,14 +1,16 @@
-package gui;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FrontEnd {
     private JFrame  jFrame;
     private JPanel  jPanel;
     private JLabel  enterTextLabel,enterShiftValLabel;
-    private JButton encodeButton, decodeButton;
-    private JTextField message,shift;
+    private JButton encodeButton, decodeButton, clearButton;
+    private JTextField message,shift,cipherMessage;
+
+    private CaesarCipher caesarCipher = new CaesarCipher();
 
     public FrontEnd() {
         createUIComponents();
@@ -19,10 +21,12 @@ public class FrontEnd {
         jPanel = new JPanel();
         encodeButton = new JButton("Encode");
         decodeButton = new JButton("Decode");
+        clearButton = new JButton("Clear");
         enterTextLabel = new JLabel();
         enterShiftValLabel = new JLabel();
         message = new JTextField();
         shift = new JTextField();
+        cipherMessage = new JTextField();
 
         //Labels configuration
         enterTextLabel.setText("Enter Your Message :");
@@ -33,10 +37,34 @@ public class FrontEnd {
         //Text Fields configuration
         message.setBounds(160,20,250,25);
         shift.setBounds(160,50,50,25);
+        cipherMessage.setBounds(15,140,470,100);
 
         //Buttons configuration
-        encodeButton.setBounds(20,90,80,25);
-        decodeButton.setBounds(150,90,80,25);
+        encodeButton.setBounds(70,90,80,25);
+        encodeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+               cipherMessage.setText( caesarCipher.encode(message.getText(), Integer.parseInt(shift.getText())));
+            }
+        });
+
+        decodeButton.setBounds(200,90,80,25);
+        decodeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                cipherMessage.setText( caesarCipher.decode(message.getText(), Integer.parseInt(shift.getText())));
+            }
+        });
+
+        clearButton.setBounds(330,90,80,25);
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                message.setText("");
+                shift.setText("");
+                cipherMessage.setText("");
+            }
+        });
 
         //Panel configuration
         jPanel.setBorder(BorderFactory.createEmptyBorder(30,30,10,10));
@@ -45,8 +73,10 @@ public class FrontEnd {
         jPanel.add(enterShiftValLabel);
         jPanel.add(message);
         jPanel.add(shift);
+        jPanel.add(cipherMessage);
         jPanel.add(encodeButton);
         jPanel.add(decodeButton);
+        jPanel.add(clearButton);
 
 //        jPannel.setVisible(true);
 //        jPannel.add(enterTextLable);
